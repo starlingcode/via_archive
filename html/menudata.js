ca332fb24a172e52b54d43b74c1f96b2ae060ecd
Will <liquidcitymotors@gmail.com>
2018-11-25 04:56:40 -0600

    add documentation to untracked files

--- html/menudata.js
+++ html/menudata.js
@@ -80,6 +80,7 @@ var menudata={children:[
 {text:"b",url:"functions_vars_b.html#index_b"},
 {text:"c",url:"functions_vars_c.html#index_c"},
 {text:"d",url:"functions_vars_d.html#index_d"},
+{text:"e",url:"functions_vars_e.html#index_e"},
 {text:"f",url:"functions_vars_f.html#index_f"},
 {text:"g",url:"functions_vars_g.html#index_g"},
 {text:"h",url:"functions_vars_h.html#index_h"},
@@ -147,7 +148,9 @@ var menudata={children:[
 {text:"q",url:"globals_vars_q.html#index_q"},
 {text:"r",url:"globals_vars_r.html#index_r"},
 {text:"s",url:"globals_vars_s.html#index_s"},
-{text:"t",url:"globals_vars_t.html#index_t"}]},
+{text:"t",url:"globals_vars_t.html#index_t"},
+{text:"v",url:"globals_vars_v.html#index_v"},
+{text:"w",url:"globals_vars_w.html#index_w"}]},
 {text:"Enumerations",url:"globals_enum.html",children:[
 {text:"a",url:"globals_enum.html#index_a"},
 {text:"b",url:"globals_enum.html#index_b"},
