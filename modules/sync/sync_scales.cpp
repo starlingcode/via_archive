f570f3d311bbacd55f23e960aacc8db7259fb7d1
Will <liquidcitymotors@gmail.com>
2019-01-15 21:12:10 -0600

    update sync tables, fix virtual dsp header file

--- modules/sync/sync_scales.cpp
+++ modules/sync/sync_scales.cpp
@@ -4,7 +4,7 @@
 void ViaSync::initializeScales() {
    scaleArray[0][0] = &integerRatios;
    scaleArray[0][1] = &even_integers;
-   scaleArray[0][2] = &rhythm_integers;
+   scaleArray[0][2] = &mult_three_integers;
    scaleArray[0][3] = &odd_integers;
    scaleArray[1][0] = &modal_tetrads;
    scaleArray[1][1] = &seventhTetradsMinimalProgression;
@@ -14,8 +14,8 @@ void ViaSync::initializeScales() {
    scaleArray[2][1] = &tetrad_inversions;
    scaleArray[2][2] = &harmonic_entropy;
    scaleArray[2][3] = &bohlenPeircevoct;
-   scaleArray[3][0] = &simpleRhythms;
-   scaleArray[3][1] = &simpleRhythms16;
-   scaleArray[3][2] = &rhythmDivisionsReset;
-   scaleArray[3][3] = &polyResets;
+   scaleArray[3][0] = &rhythm_integers;
+   scaleArray[3][1] = &no_triplets;
+   scaleArray[3][2] = &triplets;
+   scaleArray[3][3] = &poly_div_resets;
 }
